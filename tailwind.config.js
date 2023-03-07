812efcf08f48f23493760a91b8e6344367279ef7
Jztemperor <daniel.mozsar01@gmail.com>
2023-03-07 19:32:33 +0100

    print

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
         primaryText: "hsl(32, 100%, 55%)",
         secondaryText: "hsl(0, 0%, 90%)",
         spellGreen: "hsl(155, 100%, 50%)",
+        spellGreenVisited: "hsl(155, 100%, 28%)",
         bgBlack: "hsl(0, 2%, 16%)",
         contentBg: "hsl(240, 4%, 11%)",
         stringColor: "hsl(0, 0%, 53%)",
